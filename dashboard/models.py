211c9d21e30299103a529a69a0ed488d5a321083
Faisal Hussain <muscle@outlook.com>
2021-02-17 01:43:56 +0100

    Presence file data model created

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class PresenceFile(models.Model):
+    file_name = models.CharField(max_length=200)
+    upload_date = models.DateTimeField('date uploaded')
+    processing_status = models.BooleanField()
+    comments = models.CharField(max_length=200)
