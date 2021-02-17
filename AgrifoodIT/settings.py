a988527a32259871efcfca8d615abb4e7e61dfc1
Faisal Hussain <muscle@outlook.com>
2021-02-17 01:09:52 +0100

    Implemented Bootstrap Theme SB Admin. Closes #1

--- AgrifoodIT/settings.py
+++ AgrifoodIT/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard.apps.dashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Dashboard.apps.DashboardConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath("static"),
+]
