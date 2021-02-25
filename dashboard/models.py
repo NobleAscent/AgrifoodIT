31fcd75ce536c94f15b4d924a72e97d5bd23188f
Faisal Hussain <muscle@outlook.com>
2021-02-25 12:25:13 +0100

    Files can now be uploaded but alot needs to be done to clean up

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
 class PresenceFile(models.Model):
     file_name = models.CharField(max_length=200)
     comments = models.CharField(max_length=200)
-    upload_date = models.DateTimeField('date uploaded')
+
+    # using auto_now_add renders the field un-editable in the admin
+    upload_date = models.DateTimeField('date uploaded', auto_now_add=True)
     processing_status = models.BooleanField()
 
     # The primary_key argument isn’t supported and will raise an error if used.
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .models import PresenceFile
+
 
 # Create your views here.
 def index(request):
     return render(request, 'dashboard/index.html')
 
+
 def presence(request):
     latest_presence_files = PresenceFile.objects.order_by('-upload_date')[:50]
     context = {'latest_presence_files': latest_presence_files}
     return render(request, 'dashboard/presence.html', context)
+
+
+def presence_upload(request):
+    if request.method == 'POST':
+        text_file = request.FILES.get('file')
+        PresenceFile.objects.create(file_name='test123', processing_status=False, upload=text_file)
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'error'})
