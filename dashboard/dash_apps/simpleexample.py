c09395c270e6fe4f3d676cc483669c5b70b2b168
Faisal Hussain <muscle@outlook.com>
2021-04-20 11:49:27 +0200

    chore: update gitignore & remove ignored files from being tracked
    dataSources.xml file is a sensitive file and it can contain references with credentials to db used in the ide. Several other files should also be ignored. The new gitignore is auto generated from gitignore.io.
    Furthermore making git forget a file which already has been committed requires some new commands.
    
    Refs: https://stackoverflow.com/questions/1274057/how-to-make-git-forget-about-a-file-that-was-tracked-but-is-now-in-gitignore

--- dashboard/dash_apps/simpleexample.py
+++ dashboard/dash_apps/simpleexample.py
@@ -3,51 +3,34 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 from django_plotly_dash import DjangoDash
+import plotly.express as px
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = DjangoDash('SimpleExample', external_stylesheets=external_stylesheets)
 
+df = px.data.iris()
 
 app.layout = html.Div([
-    html.H1('Square Root Slider Graph'),
-    dcc.Graph(id='slider-graph', animate=True, style={"backgroundColor": "#1a2d46", 'color': '#ffffff'}),
-    dcc.Slider(
-        id='slider-updatemode',
-        marks={i: '{}'.format(i) for i in range(20)},
-        max=20,
-        value=2,
-        step=1,
-        updatemode='drag',
+    dcc.Graph(id="scatter-plot"),
+    html.P("Petal Width:"),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=0, max=2.5, step=0.1,
+        marks={0: '0', 2.5: '2.5'},
+        value=[0.5, 2]
     ),
 ])
 
 
 @app.callback(
-               Output('slider-graph', 'figure'),
-              [Input('slider-updatemode', 'value')])
-def display_value(value):
-
-
-    x = []
-    for i in range(value):
-        x.append(i)
-
-    y = []
-    for i in range(value):
-        y.append(i*i)
-
-    graph = go.Scatter(
-        x=x,
-        y=y,
-        name='Manipulate Graph'
-    )
-    layout = go.Layout(
-        paper_bgcolor='#27293d',
-        plot_bgcolor='rgba(0,0,0,0)',
-        xaxis=dict(range=[min(x), max(x)]),
-        yaxis=dict(range=[min(y), max(y)]),
-        font=dict(color='white'),
-
-    )
-    return {'data': [graph], 'layout': layout}
+    Output("scatter-plot", "figure"),
+    [Input("range-slider", "value")])
+def update_bar_chart(slider_range):
+    low, high = slider_range
+    mask = (df['petal_width'] > low) & (df['petal_width'] < high)
+    fig = px.scatter(
+        df[mask], x="sepal_width", y="sepal_length",
+        color="species", size='petal_length',
+        hover_data=['petal_width'])
+    return fig
