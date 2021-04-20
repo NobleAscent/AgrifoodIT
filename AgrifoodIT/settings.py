fb0b3a076c85199e9b2550b21fc4ed6c0d8d43e6
Faisal Hussain <muscle@outlook.com>
2021-04-20 21:26:28 +0200

    feat(dashboard): Celery task implemented
    Celery does not work on windows unless the --pool=solo option is selected in the arguments of the worker. Using this & a shared_task all files until today of Presence type were processed at a rate of 1 file per second.

--- AgrifoodIT/settings.py
+++ AgrifoodIT/settings.py
@@ -154,16 +154,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / '/media/'
 
 # Celery Configuration Options
-CELERY_TASK_TRACK_STARTED = True
-CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_RESULT_BACKEND = 'django-cache'
-# celery setting.
-CELERY_CACHE_BACKEND = 'default'
-# django setting.
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'my_cache_table',
-    }
-}
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Budapest'
--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -1,7 +1,18 @@
-# Create your tasks here
-from celery import shared_task, task
+from celery import shared_task
 
+from dashboard.models import PresenceFile
+from dashboard.processing.presence import process_presence_file
 
-@task()
-def printHello():
-    return "Hello"
+# celery -A AgrifoodIT worker -l info
+
+@shared_task()
+def processNewFiles():
+    latest_unprocessed_files = PresenceFile.objects.filter(processing_status=False)
+    print(f'Found {len(latest_unprocessed_files)} files to process')
+    for file in latest_unprocessed_files:
+        print('')
+        print(f'===== {file.file_name} =====')
+        process_presence_file(file.id)
+        print(f'===== ====== =====')
+
+    return f'Completed processing {len(latest_unprocessed_files)} files'
