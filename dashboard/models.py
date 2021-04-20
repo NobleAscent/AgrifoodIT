b0e3f5c5f9ba7a01129ebeb5d79fa7b78d0a0b00
Faisal Hussain <muscle@outlook.com>
2021-04-20 22:40:01 +0200

    feat(dashboard): added weather models.py and made file_name field unique
    Foreign key on PresenceFile table did not cascade since transfer of db from sqlite to postgres. So I had to manually change the foreign key. Before unique, user could upload the same file again and again.

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class PresenceFile(models.Model):
-    file_name = models.CharField(max_length=200)
+    file_name = models.CharField(max_length=200, unique=True)
     comments = models.CharField(max_length=200)
 
     # using auto_now_add renders the field un-editable in the admin
@@ -44,3 +44,29 @@ class Presence(models.Model):
     pig_rfid = models.ForeignKey(Pig, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
     presence_file = models.ForeignKey(PresenceFile, on_delete=models.CASCADE)
+
+# Create your models here.
+class WeatherFile(models.Model):
+    file_name = models.CharField(max_length=200, unique=True)
+    comments = models.CharField(max_length=200)
+
+    # using auto_now_add renders the field un-editable in the admin
+    upload_date = models.DateTimeField('date uploaded', auto_now_add=True)
+    processing_status = models.BooleanField()
+
+    # The primary_key argument isn’t supported and will raise an error if used.
+    # https://docs.djangoproject.com/en/3.1/ref/models/fields/#filefield
+    # file will be uploaded to MEDIA_ROOT/uploads/weather/
+    upload = models.FileField(upload_to='uploads/weather/')
+
+    def __str__(self):
+        return self.file_name
+
+class Weather(models.Model):
+    # Example of a sample line in a presence.txt file
+    # temperature: -9.38°C, pressure: 1115.44hPa, humidity:  59.55%, time: 2021-02-12T00:00:29.285 \n
+    temperature = models.FloatField()
+    pressure = models.FloatField()
+    humidity = models.FloatField()
+    timestamp = models.DateTimeField()
+    weather_file = models.ForeignKey(WeatherFile, on_delete=models.CASCADE)
