54c2a6fd8a7d47aad252c3b64991d8c3d48f0e70
Faisal Hussain <muscle@outlook.com>
2021-04-22 03:13:16 +0200

    fix(analysis): remove reference to not implemented rohit package
    Trying to fix CI failure due to non existent package

--- analysis/views.py
+++ analysis/views.py
@@ -1,9 +1,4 @@
 from django.http import JsonResponse
 
-from analysis.rohit.pig_enhanced_FPmining import generate_dataset_fpmining
-
-
 def pig_enhanced_FPmining(request):
-    generate_dataset_fpmining(num_of_pressure_ranges=5, num_of_hum_ranges=5, num_of_temp_ranges=5, time_range=180,
-                              support_threshold=0.1, conf_threshold=0.1, lift_threshold=0.1)
     return JsonResponse({'status': 'success'})
